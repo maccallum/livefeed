0f602d6405d6d4496fa4075d84cae4b70ab01ac2
John MacCallum <john.m@ccallum.com>
2022-04-25 13:17:39 +0200

    minor modifications to startall.sh and stopall.sh

--- stopall.sh
+++ stopall.sh
@@ -11,6 +11,8 @@ fi
 
 for nn in $(seq 0 "$n")
 do
-    ssh -A -t rpib.local ssh -A -t "jn${nn}.local" 'killall gst-launch-1.0'
-    ssh -A -t rpib.local ssh -A -t "jn${nn}.local" 'screen -X -S cameras quit'
+    # workaround; for some reason, killall gst-launch-1.0 && screen -X -S cameras quit
+    # says that a screen session can't be found even though it exists
+    ssh -A -t rpib.local ssh -A -t "jn${nn}.local" "killall gst-launch-1.0"
+    ssh -A -t rpib.local ssh -A -t "jn${nn}.local" "screen -X -S cameras quit"
 done
